48374a3f6d4733a2a38e919d6dfdda532136534b
Ilja Iwas <ilja@iwascoding.com>
2013-04-02 15:59:59 +0200

    Initial Commit

---
+++ cocoahubSampleCGI/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  cocoahubSampleCGI
+//
+//  Created by ilja on 02.04.13.
+//  Copyright (c) 2013 iwascoding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
